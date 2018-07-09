33d6a35d522946226a41d90093ccbd7d73f63d3b
William Peroche <williamp@wearexd.com>
2018-07-09 19:19:17 +1200

    first

---
+++ TableViewCell/MyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTableViewCell.swift
+//  TableViewCell
+//
+//  Created by William Peroche on 9/07/18.
+//  Copyright © 2018 William Peroche. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
